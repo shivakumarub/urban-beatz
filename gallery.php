235601a3cd08335dfe28115a18bca19b2566f0b9
rockh03 <39186762+rockh03@users.noreply.github.com>
2019-01-18 14:06:55 +1000

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,225 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Urban Beatz | Home :: Urban Beatz</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //custom-theme -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<link href='css/simplelightbox.min.css' rel='stylesheet' type='text/css'>
+<!-- font-awesome-icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome-icons -->
+<link href="//fonts.googleapis.com/css?family=Sofia" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Prompt:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,thai,vietnamese" rel="stylesheet"></head>
+	
+<body >
+
+<?php  include"header.php";?>
+
+<!-- breadcrumbs -->
+	<div class="breadcrumbs">
+		<div class="container">
+			<div class="w3l_breadcrumbs_left">
+				<ul>
+					<li><a href="index.php">Home</a><i>/</i></li>
+					<li>Gallery</li>
+				</ul>
+			</div>
+			<div class="w3_agile_breadcrumbs_right">
+				<h2>Gallery</h2>
+				<p>Quisque faucibus vel leo a luctus.</p>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- //breadcrumbs -->
+<!-- gallery -->
+	<div class="latest-albums">
+		<div class="container">
+			<h3 class="agileits_w3layouts_head">Our Latest <span>Gallery</span></h3>
+			<p class="w3_agileits_para">Quisque faucibus vel leo a luctus.</p>
+			<div class="wthree_latest_albums_grids gallery">
+				<div class="col-md-4 agile_gallery_grid">
+					<div class="agileits_w3layouts_gallery_grid w3_agileits_gallery_grid">
+						<a href="images/15.jpg">
+							<img src="images/15.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid">
+						<a href="images/8.jpg">
+							<img src="images/8.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid">
+						<a href="images/9.jpg">
+							<img src="images/9.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 agile_gallery_grid">
+					<div class="agileits_w3layouts_gallery_grid">
+						<a href="images/2.jpg">
+							<img src="images/2.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid w3_agileits_gallery_grid">
+						<a href="images/13.jpg">
+							<img src="images/13.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid">
+						<a href="images/3.jpg">
+							<img src="images/3.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 agile_gallery_grid">
+					<div class="agileits_w3layouts_gallery_grid">
+						<a href="images/10.jpg">
+							<img src="images/10.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid">
+						<a href="images/9.jpg">
+							<img src="images/9.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid w3_agileits_gallery_grid">
+						<a href="images/14.jpg">
+							<img src="images/14.jpg" alt=" " class="img-responsive" />
+							<div class="caption">
+								<div class="blur"></div>
+								<div class="caption-text">
+									<h3>Symphony</h3>
+									<p>Quisque faucibus vel luctus.</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //gallery -->	
+<!-- gallery-pop-up -->
+	<script type="text/javascript" src="js/simple-lightbox.js"></script>
+	<script>
+		$(function(){
+			var $gallery = $('.gallery a').simpleLightbox();
+
+			$gallery.on('show.simplelightbox', function(){
+				console.log('Requested for showing');
+			})
+			.on('shown.simplelightbox', function(){
+				console.log('Shown');
+			})
+			.on('close.simplelightbox', function(){
+				console.log('Requested for closing');
+			})
+			.on('closed.simplelightbox', function(){
+				console.log('Closed');
+			})
+			.on('change.simplelightbox', function(){
+				console.log('Requested for change');
+			})
+			.on('next.simplelightbox', function(){
+				console.log('Requested for next');
+			})
+			.on('prev.simplelightbox', function(){
+				console.log('Requested for prev');
+			})
+			.on('nextImageLoaded.simplelightbox', function(){
+				console.log('Next image loaded');
+			})
+			.on('prevImageLoaded.simplelightbox', function(){
+				console.log('Prev image loaded');
+			})
+			.on('changed.simplelightbox', function(){
+				console.log('Image changed');
+			})
+			.on('nextDone.simplelightbox', function(){
+				console.log('Image changed to next');
+			})
+			.on('prevDone.simplelightbox', function(){
+				console.log('Image changed to prev');
+			})
+			.on('error.simplelightbox', function(e){
+				console.log('No image found, go to the next/prev');
+				console.log(e);
+			});
+		});
+	</script>
+<!-- //gallery-pop-up -->
+
+<?php include"footer.php"; ?>
+
+
+
+</body>
+</html>
